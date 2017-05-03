c46b92c8b31c5b6297f1c0c433ca64fcc758c19c
Elliott Shugerman <eeshugerman@gmail.com>
2017-05-03 01:43:49 -0600

    todo lists, misc

--- dceTester.py
+++ dceTester.py
@@ -7,8 +7,13 @@ from DCE.DCEMovies_helper import frames_to_movie
 from DCE.DCEMovies import vary_tau, slide_window, compare_multi, compare_vary_tau, compare_multi_auto_tau, compare_multi_auto
 
 
-test = 2
-# test = int(sys.argv[1])
+# TODO: test/debug compare_multi_auto(), replace compare_multi_auto_tau() and compare_multi()
+# TODO: embed() wrapper
+# TODO: fix errors on Nikki's computer
+
+
+# test = 2
+test = int(sys.argv[1])
 
 print 'running test %d...' % test
 
@@ -24,11 +29,13 @@ if test == 1:
 		print 'Hello'
 		note = i + 10
 		piano = 'C134C'
-		vary_tau('datasets/time_series/%s/%s-%s.txt' % (piano, str(note), piano),
-				 tau_lims=(1, 100),
-				 tau_inc=5,
-				 embed_crop=(.5*i, .5*(i+1)),  # aka window position, in seconds
-				 ds_rate=20)             # downsample rate (takes every third sample)
+		vary_tau(
+			'datasets/time_series/%s/%s-%s.txt' % (piano, str(note), piano),
+			tau_lims=(1, 100),
+			tau_inc=5,
+			embed_crop=(.5*i, .5*(i+1)),  	# aka window position, in seconds
+			ds_rate=20
+		)             		# downsample rate (takes every third sample)
 		print 'hi'
 		frames_to_movie('output/DCE/vary_tau_%s_%s.mp4' % (str(note), piano), framerate=1)
 
@@ -37,24 +44,27 @@ if test == 2:
 	for i in xrange(7):
 		note = i*10+10
 		piano = 'C134C'
-		slide_window('datasets/time_series/%s/%s-%s.txt' % (piano, str(note), piano),
-					 window_size=.05*(i+1),    # seconds
-					 ds_rate=1,
-					 tau= 10,
-					 step_size=1)      # how much to move window each frame
-		frames_to_movie('output/DCE/slide_window_scale_tau_%s_%s.mp4' % (str(note),piano), framerate=1)
+		slide_window(
+			'datasets/time_series/%s/%s-%s.txt' % (piano, str(note), piano),
+			window_size=.05*(i+1),    # seconds
+			ds_rate=1,
+			tau=10,
+			step_size=1)      # how much to move window each frame
+		frames_to_movie('output/DCE/slide_window_scale_tau_%s_%s.mp4' % (str(note), piano), framerate=1)
 
 
 if test == 3:
 	for i in xrange(7):
 		note = (i+1)*10
 		print 'note is %s ' % str(note)
-		compare_vary_tau('datasets/time_series/C135B/%s-C135B.txt' % str(note),
-						 'datasets/time_series/C134C/%s-C134C.txt' % str(note),
-						 tau_lims=(1,40),
-						 tau_inc=2,
-						 embed_crop=(.5,.7),
-						 ds_rate = 5)
+		compare_vary_tau(
+			'datasets/time_series/C135B/%s-C135B.txt' % str(note),
+			'datasets/time_series/C134C/%s-C134C.txt' % str(note),
+			tau_lims=(1, 40),
+			tau_inc=2,
+			embed_crop=(.5, .7),
+			ds_rate=5
+		)
 		print 'note is still %s ' % str(note)
 		frames_to_movie('output/DCE/compare_tau_%s.mp4' % str(note), framerate=1)
 
@@ -63,10 +73,11 @@ if test == 4:
 	dir1 = "datasets/time_series/C134C"
 	dir2 = "datasets/time_series/C135B"
 	tau = 10
-	compare_multi(dir1, '-C134C.txt',
-				  dir2, '-C135B.txt',
-				  tau,
-				  embed_crop=(.5, .7))
+	compare_multi(
+		dir1, '-C134C.txt',
+		dir2, '-C135B.txt',
+		tau,
+		embed_crop=(.5, .7))
 	frames_to_movie('output/DCE/compare_multi_test.mp4', framerate=1)
 
 
@@ -74,12 +85,15 @@ if test == 5:
 	dir1 = "datasets/time_series/C134C"
 	dir2 = "datasets/time_series/C135B"
 	tau_T = 1/math.pi
-	compare_multi_auto_tau(dir1, '-C134C.txt',
-						   dir2, '-C135B.txt',
-						   tau_T,
-						   embed_crop=(1, 1.1),
-						   ds_rate=5,
-						   i_lims=(20, 35))
+
+	compare_multi_auto_tau(
+		dir1, '-C134C.txt',
+		dir2, '-C135B.txt',
+		tau_T,
+		embed_crop=(1, 1.1),
+		ds_rate=5,
+		i_lims=(20, 35)
+	)
 
 	frames_to_movie('output/DCE/compare_auto_tau__1_pi_t5.mp4', framerate=1)
 
@@ -91,51 +105,49 @@ if test == 7:
 	window = (1, 1.5)
 	tau_T = 1/math.pi     # set tau / period ratio
 
-	compare_multi_auto_tau(dir1, base1,
-						   dir2, base2,
-						   tau_T,
-						   embed_crop=window,
-						   ds_rate=1,
-						   i_lims=(36, 64),  # specify note range
-						   dpi=250
-						   )
+	compare_multi_auto_tau(
+		dir1, base1,
+		dir2, base2,
+		tau_T,
+		embed_crop=window,
+		ds_rate=1,
+		i_lims=(36, 64),  # specify note range
+		dpi=250
+	)
 
 	frames_to_movie('output/DCE/viol_test_7_tau.25T.mp4', framerate=1)
 
 if test == 8:
 	# still trying to figure out exactly how the units should work here
-	plot_power_spectrum('datasets/time_series/C134C/34-C134C.txt',
-						'output/DCE/power_spectrum_34-C134C.png',
-						crop=(1, 2),    # window for analysis (seconds)
-						)
+	plot_power_spectrum(
+		'datasets/time_series/C134C/34-C134C.txt',
+		'output/DCE/power_spectrum_34-C134C.png',
+		crop=(1, 2),    # window for analysis (seconds)
+	)
 
 
 if test == 9:
 	dir1, base1 = 'datasets/time_series/C134C', '-C134C.txt'
 	dir2, base2 = "datasets/time_series/viol_data", '-viol.txt'
 
-
 	compare_multi_auto(
 		dir1, base1,
 		dir2, base2,
 
-		i_lims=(36, 64), 	 # specify note range
+		i_lims=(36, 64), 		 # specify note range
 
 		embed_crop_1=(1, 2),	 # seconds or 'auto'
 		embed_crop_2=(1, 2),	 # seconds or 'auto'
-		crop_auto_len=(.3),  # for when embed_crop = 'auto'
+		crop_auto_len=.3,  	 # for when embed_crop = 'auto'
 
+		tau='auto ideal', 		 # seconds 'auto detect' or 'auto ideal'
+		tau_T=1 / math.pi, 		 # tau_sec = period * tau_T
 
-		tau='auto ideal', 	 # seconds 'auto detect' or 'auto ideal'
-		tau_T=1 / math.pi, 	 # tau_sec = period * tau_T
-
-		save_worms=True,	 # to output/DCE/saved_worms
-		save_movie=True		 # False for faster worm creation
+		save_worms=True,		 # to output/DCE/saved_worms
+		save_movie=True			 # False for faster worm creation
 	)
 
 	frames_to_movie('output/DCE/viol_test_7_tau.25T.mp4', framerate=1)
-
-
 
 
 
--- prfcTester.py
+++ prfcTester.py
@@ -5,30 +5,34 @@ from PersistentHomology.TestingFunctions import parameter_set
 from PRFCompare.PRF import PRF_dist_plots, mean_PRF_dist_plots
 from PRFCompare.PRF import mean_PRF_dist_plots
 
+# TODO: auto tau, add PRF contour plot, weighting functions
 
-# test = int(sys.argv[1])
-test = 3
+
+test = int(sys.argv[1])
+# test = 3
 
 if test == 1:
 	params = parameter_set
 	# params = standard_parameter_set
 	params.update(
 		{
-			'ds_rate' : 50,
-			'worm_length' : 5000,
+			'ds_rate': 50,
+			'worm_length': 5000,
 			'max_filtration_param': -10,
-			'num_divisions' : 50,
-		})
+			'num_divisions': 50,
+		}
+	)
 
 	i_ref = 35
 	i_arr = np.arange(20, 40, 2)
-	dir = 'datasets/embedded/test_cases'
+	direc = 'datasets/embedded/test_cases'
 	base_filename = 'L63_x_m2_tau'
 	filename_format = 'base i'				# 'i base' or 'base i'
 
 	out_filename = 'output/PRFCompare/distances1.png'
 
-	PRF_dist_plots(dir, base_filename, filename_format, out_filename, i_ref, i_arr, params,
+	PRF_dist_plots(
+		direc, base_filename, filename_format, out_filename, i_ref, i_arr, params,
 		PD_movie_int=0		# interval to build filt movies and PDs. 0 means no PDs or movies.
 	)
 
@@ -51,14 +55,13 @@ if test == 2:
 		'output/PRFCompare/C135B_vs_C134C.png',			# out filename
 		params,
 		crop_1=(2, 2.3),		# seconds or 'auto
-		crop_2 = (2, 2.3),		# seconds or 'auto
+		crop_2=(2, 2.3),		# seconds or 'auto
 		window_size=.05,		# seconds
 		num_windows=10,			# evenly spaced
 		mean_samp_num=5,		# number of windows to use for mean
 		tau=.0011,  			# seconds
 		PD_movie_int=7,			# interval to build filt movies and PDs. 0 means no PDs or movies.
 
-		# TODO: auto tau, auto crop, add PRF contour plot, normalize volume
 	)
 
 if test == 3:
@@ -78,14 +81,13 @@ if test == 3:
 		'datasets/time_series/C135B/49-C135B.txt',  # input (right)
 		'output/PRFCompare/C134C_vs_C135B_new.png',  # out filename
 		params,
-		crop_1=(2, 2.3),	# seconds or 'auto'
-		crop_2=(2, 2.3),	# seconds or 'auto'
-		crop_auto_len=.3,	# seconds. length of windows when crop is 'auto'
-		window_size=.05, 	# seconds
-		num_windows=10, 	# evenly spaced
-		mean_samp_num=10,  	# number of windows to use for mean
-		tau=.001134,  		# seconds
-		PD_movie_int=0,  	# interval to build filt movies and PDs. 0 means no PDs or movies.
+		crop_1=(2, 2.3),		# seconds or 'auto'
+		crop_2=(2, 2.3),		# seconds or 'auto'
+		crop_auto_len=.3,		# seconds. length of windows when crop is 'auto'
+		window_size=.05, 		# seconds
+		num_windows=10, 		# evenly spaced
+		mean_samp_num=10,  		# number of windows to use for mean
+		tau=.001134,  			# seconds
+		PD_movie_int=0,  		# interval to build filt movies and PDs. 0 means no PDs or movies.
 
-		# TODO: auto tau, add PRF contour plot, weighting function
 	)
