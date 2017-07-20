PRFC plot_variances(): fixed  see samples bug, changed plot style from lines -> points, fixed heatmap/PRF normalization

PH/Data.py
@@ -7,7 +7,9 @@ import subprocess
 import numpy as np
 import itertools
 
+from Utilities import blockPrint, enablePrint
 import BuildFiltration
+
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -90,7 +92,6 @@ class Filtration:
 		start_time = time.time()
 		np.savetxt('temp_data/worm_data.txt', self.sig)
 
-		from Utilities import blockPrint, enablePrint
 		try:
 			if silent: blockPrint()
 			filtration = BuildFiltration.build_filtration('temp_data/worm_data.txt', params, silent=silent)
@@ -197,7 +198,6 @@ class Filtration:
 			os.chdir('..')
 
 		def load_perseus_out_file():
-			print os.getcwd()
 			self.intervals = np.loadtxt('perseus/perseus_out_1.txt', ndmin=1)
 			if len(self.intervals) == 0:
 				print 'WARNING: no homology for this window!'

prfcTester.py
@@ -537,7 +537,7 @@ if test == 16:
 		('d_orientation_amplify', np.arange(0, 30, 3)),			# vary param 1
 		('use_cliques', (True, False)),							# vary param 2
 
-		load_saved_PRFs=False,
+		load_saved_PRFs=True,
 
 		time_units='samples',
 
@@ -555,7 +555,7 @@ if test == 16:
 
 		normalize_volume=True,
 
-		see_samples=0,  # interval to build filt movies and PDs. 0 means no PDs or movies.
+		see_samples=1,  # interval to build filt movies and PDs. 0 means no PDs or movies.
 		quiet=False
 
 	)