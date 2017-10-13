ec4dc4bf920eb69576b43e48887cc938a9476830
Elliott Shugerman <eeshugerman@gmail.com>
2017-10-13 13:35:23 -0600

    BaseTrajectory done. not tested.

--- Signals/Data.py
+++ Signals/Data.py
@@ -1,58 +1,78 @@
 import numpy as np
 
+
 class BaseTrajectory:
 
-    def __init__(
-            self, data,
-            fname=None,
-            crop=None,
-            num_windows=1,
-            window_length=None,
-            normalize_volume=None,      # 'window', 'crop', 'full' or None
+	def __init__(
+			self, data,
+			fname=None,
+			crop=None,
+			num_windows=None,
+			window_length=None,
+			vol_norm=(False, False, False),     # (full, crop, windows)
 
 
-    ):
-        if isinstance(data, basestring):        # is filename
-            self.data_full = np.loadtxt(data)
-            self.fname = data
-        else:                                   # is array
-            self.data_full = data
-            self.fname = fname
+	):
+		if isinstance(data, basestring):        # is filename
+			self.data_full = np.loadtxt(data)
+			self.fname = data
+		else:                                   # is array
+			self.data_full = data
+			self.fname = fname
 
 
-        self.norm_vol = normalize_volume
-        self.crop_lim = crop
-        self.num_windows=num_windows,
-        self.window_length=window_length
+		self.norm_vol = vol_norm
+		self.crop_lim = crop
+		self.num_windows = num_windows
+		self.window_length = window_length
 
-        self.data = self.crop()
-        self.windows, self.start_pts = self.slice()
+		if self.norm_vol[0]:
+			self.data_full = self.normalize(self.data_full)
+		self.data = self.crop(crop)
+		self.windows, self.win_start_idxs = self.slice(
+			 num_windows, window_length
+		)
 
-    def crop(self):
-        if self.crop_lim is None:
-            return self.data_full
-        else:
-            return self.data_full[self.crop_lim[0]:self.crop_lim[1]]
+	@staticmethod
+	def normalize(data):
+		return np.true_divide(data, np.max(data))
+
+	def crop(self, lim):
+		if lim is None:
+			data = self.data_full
+		else:
+			data = self.data_full[self.crop_lim[0]:self.crop_lim[1]]
+
+		if self.norm_vol[1]:
+			data = self.normalize(data)
+			self.data = data
+		return data
 
 
 
-    def slice(self):
-        # use np.slice_array
-        # start pts?
+	def slice(self, num_windows, window_length):
+		if num_windows is None:
+			return None, None
+		else:
+			start_idxs = np.floor(
+				np.linspace(0, len(self.data), num_windows)
+			).astype(int)
 
-        if self.norm_vol[2]:
-            windows = [np.true_divide(w, np.max(np.abs(w))) for w in windows]
+			if window_length is None:
+				window_length = len(self.data) / num_windows
+			windows = [self.data[sp:sp + window_length] for sp in start_idxs]
 
-        return windows, start_pts
-
-
+		if self.norm_vol[2]:
+			windows = [self.normalize(w) for w in windows]
+			self.windows, self.win_start_idxs = windows, start_idxs
+		return windows, start_idxs
 
 
 
 class TimeSeries(BaseTrajectory):
 
-    def __init__(self, data, fname=None):
-        BaseTrajectory.__init__(self, data, fname)
+	def __init__(self, data, fname=None):
+		BaseTrajectory.__init__(self, data, fname)
 
 
 
@@ -61,5 +81,5 @@ class TimeSeries(BaseTrajectory):
 
 class Trajectory(BaseTrajectory):
 
-    def __init__(self, data, fname=None):
-        BaseTrajectory.__init__(self, data, fname)
+	def __init__(self, data, fname=None):
+		BaseTrajectory.__init__(self, data, fname)
