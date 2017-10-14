5e6f8e21e121ca4938843f7d235b04874f350f49
Elliott Shugerman <eeshugerman@gmail.com>
2017-10-14 13:14:56 -0600

    unit test updates

--- unit_tests/prepare.py
+++ unit_tests/prepare.py
@@ -1,13 +1,15 @@
-from Signals import Signal
+from Signals import TimeSeries, BaseTrajectory
 import numpy as np
 
+# test_data = np.loadtxt('../datasets/time_series/clarinet/sustained/high_quality/40-clarinet-HQ.txt')
+# np.savetxt('data/40-clarinet-test.txt', test_data[:100000])
 
 # test_Signal #
-sig = Signal(
-    'data/40-clarinet-HQ.txt',
+sig = TimeSeries(
+    'data/40-clarinet-test.txt',
     crop=(1000, 10000),
     num_windows=15,
     vol_norm=(1, 1, 1)
 )
-# np.save('ref/signal.npy', sig.windows)
+np.save('ref/TS.npy', sig.windows)
 
--- unit_tests/test_all.py
+++ unit_tests/test_all.py
@@ -1,15 +1,16 @@
 import numpy as np
-from Signals import Signal
+from Signals import TimeSeries
 
 import os
 print os.getcwd()
 
-def test_Signal():
-	sig = Signal(
-		'unit_tests/data/40-clarinet-HQ.txt',
+
+def test_TS():
+	sig = TimeSeries(
+		'unit_tests/data/40-clarinet-test.txt',
 		crop=(1000, 10000),
 		num_windows=15,
 		vol_norm=(1, 1, 1)
 	)
 
-	assert np.array_equal(sig.windows, np.load('unit_tests/ref/signal.npy'))
+	assert np.array_equal(sig.windows, np.load('unit_tests/ref/TS.npy'))
