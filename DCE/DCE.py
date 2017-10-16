a910b5887bdb2e49feadb6e23eead7dc92ed9db2
Elliott Shugerman <eeshugerman@gmail.com>
2017-10-16 14:37:31 -0600

    removed extraneous options (including time_units, crop) from embed()

--- DCE/DCE.py
+++ DCE/DCE.py
@@ -4,43 +4,16 @@ from config import WAV_SAMPLE_RATE
 
 
 
-def embed(
-		signal,
-		tau,
-		m,
-		time_units='samples',
-		crop=None,
-		normalize=False,
-		normalize_crop=False,
-		ds_rate=1
-):
-	if normalize: signal = np.true_divide(signal, np.max(np.abs(signal)))
-
-	signal = signal[::ds_rate]
-
-	if crop is not None:
-		if time_units == 'samples':
-			pass
-		elif time_units == 'seconds':
-			crop = (np.array(crop) * WAV_SAMPLE_RATE).astype(int)
-		else:
-			print 'ERROR: invalid time_units'
-			sys.exit()
-		signal = signal[crop[0] : crop[1]]
-
-	if time_units == 'seconds': tau = int(tau * WAV_SAMPLE_RATE)
-
-	if normalize_crop: signal = np.true_divide(signal, np.max(np.abs(signal)))
+def embed(sig, tau, m):
 
 
-
-	end = len(signal) - (tau * (m - 1)) - 1
+	end = len(sig) - (tau * (m - 1)) - 1
 	traj = []
 
 	for i in range(end):
 		pt = []
 		for j in range(m):
-			pt.append(signal[i + (j * tau)])
+			pt.append(sig[i + (j * tau)])
 		traj.append(pt)
 
 	return np.asarray(traj)
