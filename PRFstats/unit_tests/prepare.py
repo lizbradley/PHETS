b4f185a60fc7ecdc4d2d02bcb38ff50dec4d0488
Elliott Shugerman <eeshugerman@gmail.com>
2017-11-11 22:17:03 -0700

    unit tests

--- PRFstats/unit_tests/prepare.py
+++ PRFstats/unit_tests/prepare.py
@@ -14,46 +14,54 @@ from utilities import clear_dir_force
 from PRFstats import plot_variance, L2ROCs
 from PRFstats.data import fetch_filts
 
+
+def filts2comps(fname):
+	filts = np.load(fname)
+
+	comps = np.zeros_like(filts)
+	for idx, f in np.ndenumerate(filts):
+		comps[idx] = f.complexes
+
+	out_fname = fname.replace('filts', 'comps')
+	np.save(out_fname, comps)
+
+
 def ref__fetch_filts_v():
-	fp = filt_params.copy()
 	fetch_filts(
-		ellipse_traj, fp, load_saved=False, quiet=False,
+		ellipse_traj, filt_params, load_saved=False, quiet=False,
 		out_fname='ref/ellipse_filts_v.npy',
 		vary_param_1=('ds_rate', (5, 7, 9))
 	)
 
 def data__viol_filts_v():
-	fp = filt_params.copy()
 	fetch_filts(
-		viol_traj, fp, load_saved=False, quiet=False,
+		viol_traj, filt_params, load_saved=False, quiet=False,
 		out_fname='data/viol_filts_v.npy',
 		vary_param_1=('ds_rate', np.arange(80, 150, 10))
 	)
 
 def data__clar_filts_v():
-	fp = filt_params.copy()
 	fetch_filts(
-		clar_traj, fp, load_saved=False, quiet=False,
+		clar_traj, filt_params, load_saved=False, quiet=False,
 		out_fname='data/clar_filts_v.npy',
 		vary_param_1=('ds_rate', np.arange(80, 150, 10))
 	)
 
 def data__viol_filts_vv():
-	fp = filt_params.copy()
 	fetch_filts(
-		viol_traj, fp, load_saved=False, quiet=False,
+		viol_traj, filt_params, load_saved=False, quiet=False,
 		out_fname='data/viol_filts_vv.npy',
 		vary_param_1=('ds_rate', np.arange(80, 150, 10)),
 		vary_param_2=('max_filtration_param', (-5, -6, -7)),
 	)
 
 def ref__L2ROCs():
-	fp = filt_params.copy()
+
 	out = L2ROCs(
 		clar_traj, viol_traj,
 		'clarinet', 'viol',
 		'output/L2ROCs.png',
-		fp,
+		filt_params,
 		vary_param=('ds_rate', np.arange(80, 150, 10)),
 		k=(0, 5.01, .1),
 		quiet=False,
@@ -65,14 +73,12 @@ def ref__L2ROCs():
 
 def ref__plot_variance():
 
-	fp = filt_params.copy()
-
 	clear_dir_force('output')
 
 	out = plot_variance(
 		viol_traj,
 		'output/plot_variance.png',
-		fp,
+		filt_params,
 		vary_param_1=('ds_rate', np.arange(80, 150, 10)),
 		vary_param_2=('max_filtration_param', (-5, -6, -7)),
 		quiet=False,
@@ -86,7 +92,8 @@ def ref__plot_variance():
 
 
 if __name__ == '__main__':
-	ref__fetch_filts_v()
+	# ref__fetch_filts_v()
+	print 'HDFSDFLSDFSFSFS'
 	data__viol_filts_v()
 	data__clar_filts_v()
 	data__viol_filts_vv()
