3e80dc20d4c71cbf236a43d7a96fadb6fd0f3a62
Elliott Shugerman <eeshugerman@gmail.com>
2017-11-18 11:57:04 -0700

    EmbedError for tau

--- DCE/DCE.py
+++ DCE/DCE.py
@@ -1,14 +1,22 @@
 import numpy as np
 
-def embed(signal, tau, m):
+class EmbedError(Exception):
+	def __init__(self, msg):
+		Exception.__init__(self, msg)
 
-	end = len(signal) - (tau * (m - 1)) - 1
+def embed(data, tau, m):
+
+	end = len(data) - (tau * (m - 1)) - 1
+
+	if abs(end) > len(data):
+		raise EmbedError('check tau and time_units')
+
 	traj = []
 
 	for i in range(end):
 		pt = []
 		for j in range(m):
-			pt.append(signal[i + (j * tau)])
+			pt.append(data[i + (j * tau)])
 		traj.append(pt)
 
 	return np.asarray(traj)
