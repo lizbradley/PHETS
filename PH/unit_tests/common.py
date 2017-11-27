41fb7c663b85fe44ce29b4247e1bcb0fe019d633
Elliott Shugerman <eeshugerman@gmail.com>
2017-11-27 10:22:05 -0700

    fix Filtration calls in unit tests (set save=False)

--- PH/unit_tests/common.py
+++ PH/unit_tests/common.py
@@ -13,7 +13,7 @@ euc_params.update(
 		'max_filtration_param': -5,
 		'num_divisions': 5,
 	})
-euc_filt = Filtration(ellipse_traj, euc_params)
+euc_filt = Filtration(ellipse_traj, euc_params, save=False)
 
 
 ham5_params = filt_params.copy()
@@ -24,7 +24,7 @@ filt_params.update(
 		'num_divisions': 5,
 		'd_use_hamiltonion': 5,
 	})
-ham5_filt = Filtration(ellipse_traj, ham5_params)
+ham5_filt = Filtration(ellipse_traj, ham5_params, save=False)
 
 
 doamp5_params = filt_params.copy()
@@ -35,7 +35,7 @@ doamp5_params.update(
 		'num_divisions': 5,
 		'd_orientation_amplify': 5,
 	})
-doamp5_filt = Filtration(ellipse_traj, doamp5_params)
+doamp5_filt = Filtration(ellipse_traj, doamp5_params, save=False)
 
 
 m2d10_params = filt_params.copy()
@@ -47,7 +47,7 @@ m2d10_params.update(
 		'd_use_hamiltonion': 0,
 		'm2_d': 10,
 	})
-m2d10_filt = Filtration(ellipse_traj, m2d10_params)
+m2d10_filt = Filtration(ellipse_traj, m2d10_params, save=False)
 
 
 dcov20_params = filt_params.copy()
@@ -59,7 +59,7 @@ dcov20_params.update(
 		"d_cov": 20,
 
 	})
-dcov20_filt = Filtration(ellipse_traj, dcov20_params)
+dcov20_filt = Filtration(ellipse_traj, dcov20_params, save=False)
 
 
 dcovn20_params = filt_params.copy()
@@ -70,7 +70,7 @@ dcovn20_params.update(
 		'num_divisions': 5,
 		"d_cov": -20,
 	})
-dcovn20_filt = Filtration(ellipse_traj, dcovn20_params)
+dcovn20_filt = Filtration(ellipse_traj, dcovn20_params, save=False)
 
 
 gi_params = filt_params.copy()
@@ -81,7 +81,7 @@ gi_params.update(
 		'num_divisions': 5,
 		'graph_induced': True
 	})
-gi_filt = Filtration(ellipse_traj, gi_params)
+gi_filt = Filtration(ellipse_traj, gi_params, save=False)
 
 
 hamn10_params = filt_params.copy()
@@ -93,7 +93,7 @@ hamn10_params.update(
 		'd_use_hamiltonian': -10
 
 	})
-hamn10_filt = Filtration(ellipse_traj, hamn10_params)
+hamn10_filt = Filtration(ellipse_traj, hamn10_params, save=False)
 
 
 hamn1_params = filt_params.copy()
@@ -105,7 +105,7 @@ hamn1_params.update(
 		'd_use_hamiltonian': -1
 
 	})
-hamn1_filt = Filtration(ellipse_traj, hamn1_params)
+hamn1_filt = Filtration(ellipse_traj, hamn1_params, save=False)
 
 
 ham1_params = filt_params.copy()
@@ -117,4 +117,4 @@ ham1_params.update(
 		'd_use_hamiltonian': 1
 
 	})
-ham1_filt = Filtration(ellipse_traj, ham1_params)
+ham1_filt = Filtration(ellipse_traj, ham1_params, save=False)
